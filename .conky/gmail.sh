27ff9f45df6ec58759ef2e1d23434d58dc8bdda7
Chahat Deep Singh <chahat@umd.edu>
2017-05-17 11:47:19 -0400

    Update gmail.sh

--- .conky/gmail.sh
+++ .conky/gmail.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-gmail_login="login" #login do skrzyki
-gmail_password="pass" #haslo do konta
+gmail_login="login" # Enter your Log-in Details.
+gmail_password="pass" # Enter your gMail Password here.
 
 dane="$(wget --secure-protocol=TLSv1 --timeout=3 -t 1 -q -O - \
 https://${gmail_login}:${gmail_password}@mail.google.com/mail/feed/atom \
