322690f4c698ab65955f068ab64402d96944c2ef
Chahat Deep Singh <chahat@umd.edu>
2017-12-07 13:59:24 +0000

    Update install_cuda.sh

--- nvidia-TX2-scripts/install_cuda.sh
+++ nvidia-TX2-scripts/install_cuda.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 
-#Download cuda-l4t:
-wget http://developer.download.nvidia.com/embedded/L4T/r23_Release_v1.0/cuda-repo-l4t-r23.1-7-0-local_7.0-71_armhf.deb
+#Download cuda-l4t: (arm64 only; not avaiable @developer.nvidia.com/embedded/downloads
+wget --no-check-certificate http://developer.download.nvidia.com/devzone/devcenter/mobile/jetpack_l4t/006/linux-x64/cuda-repo-l4t-8-0-local_8.0.34-1_arm64.deb
+sudo apt update
+sudo apt search cuda
+
 
