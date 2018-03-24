d5c5d43a9de4659747215f2fe32f4fa674982faf
Chahat Deep Singh <chahat@umd.edu>
2018-03-24 14:54:43 -0400

    Create redirect.js

---
+++ html-redirecting/redirect.js
@@ -0,0 +1,6 @@
+($wait = $(".w8")), $wait.delay(100).fadeOut("slow");
+($error = $(".error")), $error.delay(1000).fadeIn("slow");
+($link = $(".foth")), $link.delay(100).fadeIn("slow");
+($hr = $(".line")), $hr.delay(1350).fadeOut("slow");
+
+//Sorry for the sloppiness in the code, I did for myself. Used as a draft.
