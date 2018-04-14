5dcc0c8cbf4c4742d2fa48d53df891e488504861
Chahat Deep Singh <chahat@umd.edu>
2018-04-14 10:08:00 -0400

    Create memory_usage.sh

---
+++ .scripts/memory_usage.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+# To run script infinite times, do:
+# watch -n0 bash memory_usage.sh
+
+# Log saved to filename:
+filename=../syslog.txt # Don't push the log file to git as the size can be huge.
+
+# Log after every 'sleeptime' seconds
+sleeptime=1
+
+# Notify memory usage start:
+notify-send "Starting memory log" -i face-smile "Check '$filename' for results"
+
+# Store Date:
+date &>> $filename
+#date &>> $filename
+
+# Store RAM Usage:
+echo RAM Usage: >> $filename
+free -m &>> $filename
+
+# CPU Usage:
+(printf "CPU Usage: " & mpstat | awk '$3 ~ /CPU/ { for(i=1;i<=NF;i++) { if ($i ~ /%idle/) field=i } } $3 ~ /all/ { print 100 - $field }' ) >> $filename
+echo &>> $filename
+
+# GPU USAGE:
+(printf "GPU Usage: " & nvidia-smi | grep Default) &>> $filename
+(printf "GPU Usage: " & nvidia-smi | grep python) &>> $filename
+(printf "GPU Usage: " & nvidia-smi | grep compiz_worker) &>> $filename
+
+# Compiz_worker
+
+(echo & echo "USER       PID %CPU %MEM    VSZ   RSS TTY      STAT START   TIME COMMAND"  & ps aux | grep compiz_worker) &>> $filename
+
+# Python:
+ps aux | grep python &>> $filename
+
+echo ---------------------------------------------------------------------------&>> $filename
+echo &>> $filename
+
+
+sleep $sleeptime
